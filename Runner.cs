fb9ad7b600df9ae42a22f7d82f1a3d28b09c7f7b
Jindetta <jindetta@gmail.com>
2024-03-08 22:07:46 +0200

    Initial commit

---
+++ Runner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hCMD
+{
+    internal class Runner
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("You can't see this output due to app being in windowed mode.");
+        }
+    }
+}
