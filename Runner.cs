824f2cf2618bf1540b848eb8b13397e85f1e5682
JPimia <90615229+JPimia@users.noreply.github.com>
2024-03-12 00:18:34 +0200

    Added basic logging function

--- Runner.cs
+++ Runner.cs
@@ -1,19 +1,24 @@
-﻿using System;
+﻿using NLog;
+using System;
 
 namespace hCMD
 {
     internal class Runner
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
-        {
+        { 
+            SetupLogging();
             Console.WriteLine("You can't see this output due to app being in windowed mode.");
-
+            logger.Info("This is test message");
         }
 
         private static void SetupLogging()
         {
             // TODO: Include NLog Nuget package to project references
             // TODO: Setup NLog here
+
+            NLog.LogManager.LoadConfiguration("NLog.config");
         }
     }
 }
