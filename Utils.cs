1287c041878cb8ac9dd144c467e3eb72fc613e63
Jindetta <jindetta@gmail.com>
2024-03-13 02:15:30 +0200

    Added some potentially helpful information to Utils

--- Utils.cs
+++ Utils.cs
@@ -7,13 +7,24 @@ namespace hCMD
     {
         public static bool IncludedInPathVariable()
         {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            //var paths = ...;
+
             // TODO: Implement checking for environment PATH variable (if current directory is included)
+            // TODO: Get environment variable PATH (user and system scoped) loop through those paths to see if current dir is there
+
             return false;
         }
 
         public static void UpdatePathVariable()
         {
+            //var scope = EnvironmentVariableTarget.User;
+            var currentDirectory = Directory.GetCurrentDirectory();
+            //var paths = ...;
+
             // TODO: Include current directory in environment PATH variable
+            // TODO: Get environment variable "PATH" for User and append current path to the end (if it's not there)
+            // TODO: Replace existing environment variable "PATH" (user scoped) with new PATH
         }
     }
 }
