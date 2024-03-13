e07b2811d112fd88ce51833a781bb9b314e3e091
JPimia <90615229+JPimia@users.noreply.github.com>
2024-03-14 01:00:44 +0200

    Feature: Added method to check if current dir is included in PATH variable

--- Utils.cs
+++ Utils.cs
@@ -1,17 +1,28 @@
-﻿using System;
+﻿using NLog;
+using System;
 
 namespace hCMD
 {
     // TODO: Implement methods to check environment PATH variable contents and manipulate it
     internal class Utils
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         public static bool IncludedInPathVariable()
         {
             var currentDirectory = Directory.GetCurrentDirectory();
-
-            // TODO: Implement checking for environment PATH variable (if current directory is included)
-            // TODO: Get environment variable PATH (user and system scoped) loop through those paths to see if current dir is there
-
+            var paths = Environment.GetEnvironmentVariable("PATH");
+            if (string.IsNullOrEmpty(paths))
+            {
+                return false;
+            }
+            var splitPaths = paths.Split(';');
+            foreach (var path in splitPaths)
+            {
+                if (path == currentDirectory)
+                {
+                    return true;
+                }
+            }
             return false;
         }
 
