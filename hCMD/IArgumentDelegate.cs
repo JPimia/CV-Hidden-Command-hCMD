8978cf7393500f2818f3b808c02d9dc175e2981d
Jindetta <jindetta@gmail.com>
2024-03-27 23:16:25 +0200

    Added IArgumentDelegate interface stub

---
+++ hCMD/IArgumentDelegate.cs
@@ -0,0 +1,15 @@
+﻿namespace hCMD
+{
+    public interface IArgumentDelegate
+    {
+        public void OnRunCommand(string command, string arguments);
+
+        public void OnAddProfile(string profileName, string profileSource);
+        public void OnLoadProfile(string profileName);
+        public void OnSetupEnvironmentPath();
+
+        public void OnError(string errorMessage);
+
+        public void OnShowUsageInstructions();
+    }
+}
