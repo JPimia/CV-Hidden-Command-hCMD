0ea8dd9504bec5119c382eadba6e8e1d5cec75de
Jindetta <jindetta@gmail.com>
2024-03-27 23:15:57 +0200

    Fixed ProfileParser not catching JsonSerializationException

--- hCMD/ProfileParser.cs
+++ hCMD/ProfileParser.cs
@@ -1,12 +1,29 @@
 ﻿using Newtonsoft.Json;
+using NLog;
 
 namespace hCMD
 {
     public static class ProfileParser
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public static List<Profile>? Parse(string filePath) => ParseJson(File.ReadAllText(filePath));
 
-        public static List<Profile>? ParseJson(string jsonData) => JsonConvert.DeserializeObject<List<Profile>>(jsonData);
+        public static List<Profile>? ParseJson(string jsonData)
+        {
+            List<Profile>? profiles = null;
+
+            try
+            {
+                profiles = JsonConvert.DeserializeObject<List<Profile>>(jsonData);
+            }
+            catch (JsonSerializationException exception)
+            {
+                logger.Warn(exception);
+            }
+
+            return profiles;
+        }
 
         public static Profile? GetProfile(string filePath, string profileName) => FindProfileByName(Parse(filePath) ?? [], profileName);
 
