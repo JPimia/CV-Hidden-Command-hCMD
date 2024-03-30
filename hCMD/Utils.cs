3bbf41502d1cf46c74f359c61ad75950a471f63d
Jindetta <jindetta@gmail.com>
2024-03-30 07:41:53 +0200

    Implemented more Utility methods
    - Implemented method for finding executables in environment path

--- hCMD/Utils.cs
+++ hCMD/Utils.cs
@@ -5,6 +5,8 @@ namespace hCMD
     public static class Utils
     {
         private static readonly string PATH_VARIABLE = "PATH";
+        private static readonly string PATH_EXT_VARIABLE = "PATHEXT";
+
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
         internal static bool IncludedInPathVariable()
@@ -38,12 +40,35 @@ namespace hCMD
             Environment.SetEnvironmentVariable(PATH_VARIABLE, AppendToPathString(userPath, currentDirectory), scope);
         }
 
+        internal static bool ExecutableExistsInPathVariable(string executable)
+        {
+            var environmentPath = Environment.GetEnvironmentVariable(PATH_VARIABLE);
+            var pathExtensions = Environment.GetEnvironmentVariable(PATH_EXT_VARIABLE);
+
+            if (environmentPath == null)
+            {
+                logger.Fatal($"Environment variable named '{PATH_VARIABLE}' was not found!");
+
+                return false;
+            }
+            else if (pathExtensions == null) 
+            {
+                logger.Fatal($"Environment variable named '{PATH_EXT_VARIABLE}' was not found!");
+
+                return false;
+            }
+
+            return ExecutableExistsInPathString(environmentPath, pathExtensions, executable);
+        }
+
         public static bool IncludedInPathString(string environmentPath, string directory)
         {
             var pathToSearch = directory.NormalizePath();
 
             foreach (var path in environmentPath.Split(';'))
             {
+                if (string.IsNullOrWhiteSpace(path)) continue;
+
                 if (pathToSearch.Equals(path.NormalizePath(), StringComparison.OrdinalIgnoreCase))
                 {
                     return true;
@@ -67,6 +92,55 @@ namespace hCMD
             }
 
             return $"{environmentPath};{pathToAppend}";
+        }
+
+        public static bool IsValidFileName(string path)
+        {
+            var filename = Path.GetFileName(path).Trim();
+
+            return filename.Length > 0
+                   && !filename.EndsWith('.')
+                   && filename.IndexOfAny(Path.GetInvalidFileNameChars()) < 0;
+        }
+
+        public static bool ExecutableExistsInPathString(string environmentPath, string pathExtension, string executable)
+        {
+            if (!Path.HasExtension(executable))
+            {
+                foreach (var extension in pathExtension.Split(';'))
+                {
+                    if (ExecutableExistsInPathString(environmentPath, Path.ChangeExtension(executable, extension)))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            return ExecutableExistsInPathString(environmentPath, executable);
+        }
+
+        public static bool ExecutableExistsInPathString(string environmentPath, string executable)
+        {
+            var executablePath = Environment.ExpandEnvironmentVariables(executable);
+
+            if (!File.Exists(executablePath))
+            {
+                var executableFilename = Path.GetFileName(executablePath);
+
+                foreach (var path in environmentPath.Split(';'))
+                {
+                    if (File.Exists(Path.Combine(path.Trim(), executableFilename)))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            return true;
         }
 
         private static string NormalizePath(this string path)
